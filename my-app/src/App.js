e0c27ad009abfa2f9c8a2ff726c961ee4585fb8e
Heber <hebergonza@gmail.com>
2020-01-18 12:32:13 -0500

    created UserCard.js and already obtained fetch request for user

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import UserCard from "./Components/UserCard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      followersList: ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell', 'K-JHarris'],
+      user: {}
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://api.github.com/users/hebergonza719")
+      .then(res => res.json()) // needs to be parsed
+      .then(gitUser => { // above line turns response into json then used as "user"
+        console.log("this is user", gitUser)
+        console.log("this is followers", gitUser.followers.toString());
+        this.setState({ ...this.state, user: gitUser });
+      })
+      .catch(err => {console.log("this is error", err);})
+  }
+
+  render() {
+    return (
+      <div>
+        <UserCard gitUser={this.state.user}/>
+      </div>
+    );
+  }
 }
 
 export default App;
